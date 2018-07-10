b1712e9d7c6dfb3b5eb55a4738577fd547bf338e
itfront <itfront@163.com>
2018-07-10 16:20:18 +0800

    cordova + f7-vue(v3)

--- build/fix-static-path.js
+++ build/fix-static-path.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function () {
-  const cssFiles = fs.readdirSync('./www/static/css');
-  cssFiles.forEach((cssFile) => {
-    if (cssFile.indexOf('.') === 0) return;
-    let fileContent = fs.readFileSync(path.resolve('./www/static/css', cssFile), 'utf8');
-    fileContent = fileContent
-      .replace(/url\(static\//gi, 'url(../')
-      .replace(/url\(\/static\//gi, 'url(../');
-      fs.writeFileSync(path.resolve('./www/static/css', cssFile), fileContent);
-  });
+    const cssFiles = fs.readdirSync('./www/static/css');
+    cssFiles.forEach((cssFile) => {
+        if (cssFile.indexOf('.') === 0) return;
+        let fileContent = fs.readFileSync(path.resolve('./www/static/css', cssFile), 'utf8');
+        fileContent = fileContent
+            .replace(/url\(static\//gi, 'url(../')
+            .replace(/url\(\/static\//gi, 'url(../');
+        fs.writeFileSync(path.resolve('./www/static/css', cssFile), fileContent);
+    });
 }
